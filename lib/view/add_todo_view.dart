83e416668488603876d9c20ae2eb5ce46b2ab8a4
yorioka <hyorioka@j-skk.com>
2022-03-22 18:00:58 +0900

    add view edit

--- lib/view/add_todo_view.dart
+++ lib/view/add_todo_view.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../view_model/todo_list_view_model.dart';
 
 class AddTodoView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // 件名用
+    final titleValueController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: Text("add todo"),
       ),
       body: Column(children: [
         Row(
-          children: [Text("タイトル"), Flexible(child: TextField())],
+          children: [
+            Text("タイトル"),
+            Flexible(
+                child: TextField(
+              controller: titleValueController,
+            ))
+          ],
         ),
         Row(
           children: [Text("日付"), Flexible(child: TextField())],
         ),
         Spacer(),
         ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            print(titleValueController.text);
+          },
           child: Text("保存"),
         )
       ]),
